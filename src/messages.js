af6088ed3d6c909a6c9ba82a0e231549b838917d
Kavya <kavya.kenchegowda@indegene.com>
2022-06-20 11:05:28 +0530

    offline msg change

--- src/messages.js
+++ src/messages.js
@@ -19,14 +19,18 @@ var emailMsg = "Please provide your Email";
 var emailMsgPlaceholder = "Please enter you email here";
 var nonWorkingHoursMsg =
   "Our Live agents are currently unavailable. Live agents are only available from 8AM - 6PM EST on Working days.";
+// var nonWorkingHoursTemplate =
+//   "<div> Please come back later or explore <a class='med-info-link' href='https://www.oncologymedinfo.com/MedicalInformation' target='_blank'>www.oncologymedinfo.com</a></div>";
 var nonWorkingHoursTemplate =
-  "<div> Please come back later or explore <a class='med-info-link' href='https://www.oncologymedinfo.com/MedicalInformation' target='_blank'>www.oncologymedinfo.com</a></div>";
+  "Please come back later or look at the Takeda Med Info website.";
 var errorMsg = "Something went wrong!, please come back after some time...";
 var connectingMsg = "We are connecting you to our call centre agent.";
 var apologyMsg =
   "Apology for the delay, can you please wait till the agents are available";
+// var agentsNotAvailableTemplate =
+//   "<div> All our live agents are currently busy, please come back later or explore <a class='med-info-link' href='https://www.oncologymedinfo.com/MedicalInformation' target='_blank'>www.oncologymedinfo.com</a></div>";
 var agentsNotAvailableTemplate =
-  "<div> All our live agents are currently busy, please come back later or explore <a class='med-info-link' href='https://www.oncologymedinfo.com/MedicalInformation' target='_blank'>www.oncologymedinfo.com</a></div>";
+  "<div> All our live agents are currently busy, please come back later or look at the Takeda Med Info website.</div>";
 var agentJoinedMsg = " our call centre agent, has joined the conversation.";
 var userResponsesPlaceholder = "Please type your responses here";
 var thankYouMsg =
